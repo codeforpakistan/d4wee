193126806b87b06d9fbfc09128d0333d7371f25b
Shaji Ahmed <ahmeds@alphaxds.com>
2026-09-10 16:01:41 +0500

    Fixing for duplicate certs

--- app/management/commands/generate_certificates.py
+++ app/management/commands/generate_certificates.py
@@ -51,19 +51,16 @@ class Command(BaseCommand):
         for enrollment in enrollments:
             if enrollment.certificate_eligible:
                 count += 1
-                certificiate = Certificate(
+                certificiate = Certificate.objects.get_or_create(
                     enrollment=enrollment,
                     issued_date=timezone.localdate(),
                     completion_percentage=enrollment.completion_rate or 0,
                     average_grade=enrollment.overall_average_score,
                 )
-                if not dry_run:
-                    certificiate.save()
 
-                enrollment.status = Enrollment.StatusChoices.COMPLETED
-                enrollment.completion_date = timezone.localtime()
-
-                if not dry_run:
+                if certificiate:
+                    enrollment.status = Enrollment.StatusChoices.COMPLETED
+                    enrollment.completion_date = timezone.localtime()
                     enrollment.save()
 
                 self.stdout.write(f'{enrollment.student.email} - {enrollment.course.name} (Completion: {round(enrollment.completion_rate)}%, Grade: {round(enrollment.overall_average_score)}%)')
