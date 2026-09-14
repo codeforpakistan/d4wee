e867601397f0c9d3fae19cbb1f32b4cb4224d3b7
Shaji Ahmed <ahmeds@alphaxds.com>
2026-09-14 18:48:46 +0500

    Limiting to one course only

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -26,7 +26,7 @@ def index(request):
 
     grades = StudentReport.objects.filter(email=user.email)
     # registrations = Registration.objects.filter(student=student).prefetch_related('cohort')
-    enrollments = Enrollment.objects.prefetch_related('course').filter(registration__student__user=user)
+    enrollments = Enrollment.objects.prefetch_related('course').filter(registration__student__user=user, registration__cohort__status=Cohort.StatusChoices.ACTIVE)
     cohorts = Cohort.objects.prefetch_related(
         Prefetch('registrations', queryset=Registration.objects.filter(student=student))
     ).filter(is_open_for_registration=True)
